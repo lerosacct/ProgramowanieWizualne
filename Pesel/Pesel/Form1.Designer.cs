9abed4b564071d7d0e7db9a061503e5e7edd061e
Jaroslaw <kontakt@zadorozny.pl>
2020-03-08 13:19:51 +0100

    Add files via upload

---
+++ Pesel/Pesel/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Pesel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pesel = new System.Windows.Forms.TextBox();
+            this.PeselLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pesel
+            // 
+            this.pesel.Location = new System.Drawing.Point(51, 6);
+            this.pesel.Name = "pesel";
+            this.pesel.Size = new System.Drawing.Size(145, 20);
+            this.pesel.TabIndex = 0;
+            this.pesel.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PeselLabel
+            // 
+            this.PeselLabel.AutoSize = true;
+            this.PeselLabel.Location = new System.Drawing.Point(12, 9);
+            this.PeselLabel.Name = "PeselLabel";
+            this.PeselLabel.Size = new System.Drawing.Size(33, 13);
+            this.PeselLabel.TabIndex = 1;
+            this.PeselLabel.Text = "Pesel";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sprawdz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(84, 33);
+            this.progressBar1.Maximum = 4;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(193, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Poprawnosc";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 65);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PeselLabel);
+            this.Controls.Add(this.pesel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pesel;
+        private System.Windows.Forms.Label PeselLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pesel/Pesel/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pesel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String temp_string = pesel.Text;
+            int test_passed = 0;
+
+            if ((Convert.ToInt32(temp_string[2]) - 48) < 2)
+            {
+                progressBar1.Value = test_passed;
+                test_passed++;
+                if ((Convert.ToInt32(temp_string[4]) - 48) < 3)
+                {
+                    progressBar1.Value = test_passed;
+                    test_passed++;
+                    if (temp_string.Length == 11)
+                    {
+                        progressBar1.Value = test_passed;
+                        test_passed++;
+                        int calc = (1 * (Convert.ToInt32(temp_string[0]) - 48)) +
+                                (3 * (Convert.ToInt32(temp_string[1]) - 48)) +
+                                (7 * (Convert.ToInt32(temp_string[2]) - 48)) +
+                                (9 * (Convert.ToInt32(temp_string[3]) - 48)) +
+                                (1 * (Convert.ToInt32(temp_string[4]) - 48)) +
+                                (3 * (Convert.ToInt32(temp_string[5]) - 48)) +
+                                (7 * (Convert.ToInt32(temp_string[6]) - 48)) +
+                                (9 * (Convert.ToInt32(temp_string[7]) - 48)) +
+                                (1 * (Convert.ToInt32(temp_string[8]) - 48)) +
+                                (3 * (Convert.ToInt32(temp_string[9]) - 48));
+
+                        int lastDigit = 10 - calc % 10;
+                        if (Convert.ToInt32(temp_string[10])-48 == lastDigit)
+                        {
+                            progressBar1.Value = test_passed;
+                            test_passed++;
+                        }
+                    }
+                }
+            }
+            progressBar1.Value = test_passed;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
